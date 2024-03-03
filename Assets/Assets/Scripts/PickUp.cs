73b712fdd10026b3821bc22f21799b2465d1f539
scso-ucsc <scso@ucsc.edu>
2024-03-03 14:12:15 -0800

    Attempting To Add Pickup To Player

--- Assets/Assets/Scripts/PickUp.cs
+++ Assets/Assets/Scripts/PickUp.cs
@@ -16,23 +16,24 @@ public class PickUp: MonoBehaviour
         rb = GetComponent<Rigidbody>(); // Get the reference to the Rigidbody component attached to this GameObject
     }
 
-    void FixedUpdate()
-    {
-        // Input from arrow keys or WASD
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    // void FixedUpdate()
+    // {
+    //     // Input from arrow keys or WASD
+    //     float moveHorizontal = Input.GetAxis("Horizontal");
+    //     float moveVertical = Input.GetAxis("Vertical");
 
-        // Calculate movement direction
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    //     // Calculate movement direction
+    //     Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        // Apply movement to the Rigidbody
-        rb.AddForce(movement * speed);
-    }
+    //     // Apply movement to the Rigidbody
+    //     rb.AddForce(movement * speed);
+    // }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && carry == null)
         {
+            Debug.Log("Space Pressed"); //For Debugging
             PickUpF();
         }
         else if (Input.GetKeyDown(KeyCode.Space) && carry != null)
@@ -46,8 +47,10 @@ public class PickUp: MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(transform.position, transform.forward, out hit, 3f))
         {
-            if (hit.collider.CompareTag("Pickup"))
+            //if (hit.collider.CompareTag("Pickup"))
+            if (hit.collider.CompareTag("Glass")) //Changed to Glass for interaction with glass objects
             {
+                Debug.Log("GlassObject Hit!"); //For Debugging
                 carry = hit.collider.gameObject;
                 carry.GetComponent<Rigidbody>().isKinematic = true;
                 carry.transform.parent = transform;
