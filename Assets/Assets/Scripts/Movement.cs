29f0727b59a25de547d93bbc7d67916f83fb1f41
Akhrxr <130079398+Akhrxr@users.noreply.github.com>
2024-03-07 15:43:41 -0800

    Updated Movement to be more smooth, again
    Updated Movement to use velocity instead of force. Updated cams to keep from clipping through walls. Fixed player rotation

--- Assets/Assets/Scripts/Movement.cs
+++ Assets/Assets/Scripts/Movement.cs
@@ -4,52 +4,27 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {   
-    private float mySpeed = 12f;
-    //bool IsGrounded = true;
-    float rot = 0f;
-    float r;
+    private Vector3 MovementInput;
+    private Vector2 MouseInput; //Inputs
+    private float mySpeed = 10f; //Speed
+    private float sense = 4f; //sensitivity
+    private float xRotation;
 
     public void playerMovement(Rigidbody myRB)
     {
-        //Debug.Log(IsGrounded());
-    //Rotation
-        rotation();
-    //Movement
-        if (Input.GetAxis("Vertical") > 0) {
-            myRB.AddRelativeForce(Vector3.forward * mySpeed * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        else if (Input.GetAxis("Vertical") < 0) {
-            myRB.AddRelativeForce(Vector3.back * mySpeed * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        else
-        {
-            myRB.velocity = new Vector3(0, 0, 0);
-        }
-        if (transform.position.y < 1.2f) {
-            transform.position = new Vector3(transform.position.x, 1.2f, transform.position.z);
-        }
-        myRB.angularVelocity = Vector3.zero;
+        MovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        MouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        //Rotation
+        rotation();     
+        //Movement
+        Vector3 MoveVector = transform.TransformDirection(MovementInput) * mySpeed;
+        myRB.velocity = new Vector3(MoveVector.x, myRB.velocity.y, MoveVector.z);
+        
     }
     
     public void rotation()
     {   
-        if (Input.GetAxis("Horizontal") > 0) {
-            rot += 0.5f; //I've reduced rotation speed to not be so fast as it was when it was originally += 2f - Sean
-        }
-        else if (Input.GetAxis("Horizontal") < 0) {
-            rot += -0.5f;
-        }
-        float Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, rot, ref r, 0.1f);
-        transform.rotation = Quaternion.Euler(0, Angle, 0);
+        xRotation += MouseInput.x * sense;
+        transform.localRotation = Quaternion.Euler(0f, xRotation, 0f);
     }
-
-    /*public bool IsGrounded() //Might need later on possibly maybe hopefully maybe
-    {
-        if (Physics.Raycast(transform.position, -Vector3.up, out RaycastHit hitinfo, 2f)) {
-            if (hitinfo.collider.gameObject.transform.tag == "Floor") {
-                return true;
-            }
-        }
-        return false;
-    }*/
 }
