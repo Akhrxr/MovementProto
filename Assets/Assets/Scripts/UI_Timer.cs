add790b085cd21a43665e60b6a7860415a4bd568
scso-ucsc <scso@ucsc.edu>
2024-03-07 22:34:25 -0800

    Adding UI Timer

---
+++ Assets/Assets/Scripts/UI_Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float remainingTime = 300; //Base time limit = 3 minutes aka 300
+    private int minutes;
+    private int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Code based on https://www.youtube.com/watch?v=POq1i8FyRyQ: Make a TIMER & COUNTDOWN in 5 Mins | Unity Tutorial for Beginners 
+        if(remainingTime >= 0){
+            remainingTime -= Time.deltaTime;
+            minutes = Mathf.FloorToInt(remainingTime / 60);
+            seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = "Time Remaining: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+        if(remainingTime <= 61){ //Switch timer colour to red if less than 1 minute left
+                timerText.color = new Color(0.8f, 0f, 0f);
+        }
+    }
+}
