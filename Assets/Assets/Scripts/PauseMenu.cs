c8d1d69508651e9ec9d4961adc9fefe6337c88f6
Akhrxr <130079398+Akhrxr@users.noreply.github.com>
2024-03-10 18:02:11 -0700

    Added functioning pause menu

---
+++ Assets/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.ComponentModel;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject nonDestroyablePauseMenu;
+    public Button resumeButton;
+    public Button mainMenuButton;
+    public Button restartButton;
+    public Button exitButton;
+
+    public bool isPaused = false;
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(nonDestroyablePauseMenu);
+        TogglePauseMenu(false);
+        resumeButton.onClick.AddListener(ResumeGame);
+        mainMenuButton.onClick.AddListener(ReturnToMainMenu);
+        restartButton.onClick.AddListener(RestartGame);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1) && isPaused)
+        {
+            ResumeGame();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && isPaused)
+        {
+            RestartGame();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && isPaused)
+        {
+            ReturnToMainMenu();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && isPaused)
+        {
+            ExitGame();
+        }
+
+    }
+
+    void TogglePauseMenu(bool pause)
+    {
+        pauseMenuUI.SetActive(pause);
+        Time.timeScale = pause ? 0 : 1;
+        isPaused = pause;
+    }
+
+    void PauseGame()
+    {
+        TogglePauseMenu(true);
+    }
+
+    void ResumeGame()
+    {
+        TogglePauseMenu(false);
+    }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void RestartGame()
+    {
+        TogglePauseMenu(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Exited.");
+    }
+
+}
